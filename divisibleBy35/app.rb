d7d442bd80b5c81a8775832ac9581783177bd78d
Mike Wales <webmaster@michaelwales.com>
2023-10-14 17:58:00 -0500

    Adds more comments to divisibleBy35 RB solution

--- divisibleBy35/app.rb
+++ divisibleBy35/app.rb
@@ -1,19 +1,31 @@
+# We can grab command line arguments from the ARGV array, notably
+# Ruby does not include the initial command in this array, so this is the model:
+# -- ruby app.rb ARVG[0] ARVG[1]
+# We'll also typecase to integer right now.
 lowNumber = ARGV[0].to_i
 highNumber = ARGV[1].to_i
 
-# Make sure we have 2 parameters
+# Make sure we have 2 parameters, otherwise exit the program
 exit(1) unless ARGV[1]
 
+# Ruby is a non-typed language, so we just set two variables to track our total counts
 totalDivBy3 = 0
 totalDivBy5 = 0
 
 if (lowNumber > highNumber)
-    #If we get here, the first param is larger than the last
+    # If we get here, the first param is larger than the last
+    # Let's swap them around
     lowNumber = ARGV[1].to_i
     highNumber = ARGV[0].to_i
 end
 
 # Loop through the spread of numbers
+# Ruby has a pretty sweet integer spread operation using ..
+# So, for example 1..3.each do |num| would result in 1, 2, 3 for num
+# Here, we're using that to go through each fo the numbers between
+# low and high. In other languages, you'll probably see this 
+# implemented using a for (i = low, i <= high, i++) style loop.
+# The .. operator basically shorthands that for loop.
 (lowNumber..highNumber).each do |num|
     totalDivBy3 += 1 if num % 3 == 0
     totalDivBy5 += 1 if num % 5 == 0
